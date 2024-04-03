7fb4e1eb75afe38365679a2f302211d82144aecb
Egor Kuritsin <ekuritsin@ptsecurity.com>
2024-04-03 10:23:23 +0300

    add logging

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -8,11 +8,14 @@
 #                     '$request_time';
 import json
 import typing
+import logging
 import argparse
 
 parser = argparse.ArgumentParser(description='Log analyzer')
 parser.add_argument('--config', help='Config file path', default='config.json')
 parsed_args = parser.parse_args()
+
+logger = logging.getLogger(__name__)
 
 config = {
     "REPORT_SIZE": 1000,
@@ -22,19 +25,24 @@ config = {
 
 
 def get_config() -> dict[str, typing.Any]:
-    try:
-        with open(parsed_args.config, 'r') as f:
-            json_config = json.load(f)
-            config.update(json_config)
-    except FileNotFoundError:
-        print('file not exist')
-    except json.decoder.JSONDecodeError:
-        print('wrong format')
+    with open(parsed_args.config, 'r') as f:
+        json_config = json.load(f)
+        config.update(json_config)
     return config
+
+
+def set_logging_settings(config: dict[str, typing.Any]) -> None:
+    logging.basicConfig(
+        filename=config.get('LOG_FILE_NAME'),
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        datefmt='%Y.%m.%d %H:%M:%S',
+        level=logging.DEBUG,
+    )
 
 
 def main():
     config = get_config()
+    set_logging_settings(config=config)
 
 
 if __name__ == "__main__":
