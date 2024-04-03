320fad467446bd7b6bfa914678d5eefd67bac3f8
Egor Kuritsin <ekuritsin@ptsecurity.com>
2024-04-03 18:34:19 +0300

    main functional completed

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
-# log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
-#                     '$status $body_bytes_sent "$http_referer" '
-#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
-#                     '$request_time';
 import os
 import re
 import gzip
 import json
-import typing
 import logging
 import argparse
+from statistics import median
 from collections import namedtuple
 
 parser = argparse.ArgumentParser(description='Log analyzer')
@@ -26,7 +20,7 @@ LogFile = namedtuple('LogFile', 'name, date, extension')
 config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
-def get_config() -> dict[str, typing.Any]:
+def get_config(config: dict) -> dict:
     with open(parsed_args.config) as f:
         json_config = json.load(f)
         config.update(json_config)
@@ -56,13 +50,18 @@ def get_last_log_file(log_dir: str) -> LogFile | None:
     return last_log_file
 
 
-def analyse_log(log_dir: str, log_file: LogFile | None):
+def get_raw_log_analysis(log_dir: str, log_file: LogFile) -> tuple[dict, int, float]:
+    item_counter = 0
+    time_counter = 0
     analyzed_data = {}
-    initial_dict = dict.fromkeys(('count', 'time_avg', 'time_max', 'time_sum', 'time_med', 'time_perc', 'count_perc'), 0)
+    initial_dict = {
+        'count': 0,
+        'time_avg': 0,
+        'time_max': 0,
+        'time_sum': 0,
+        'values': [],
+    }
 
-    if log_file is None:
-        logger.info('There is no required file for log analysis')
-    
     file_open_func = gzip.open if log_file.extension == 'gz' else open
 
     with file_open_func(os.path.join(log_dir, log_file.name), mode='rt') as file:
@@ -70,24 +69,68 @@ def analyse_log(log_dir: str, log_file: LogFile | None):
             splitted_line = line.split()
             request_url = splitted_line[6]
             response_time = float(splitted_line[-1])
+            item_counter += 1
+            time_counter += response_time
 
-            url_stat = analyzed_data.setdefault(request_url, initial_dict.copy())
-            url_stat['count'] += 1
-            url_stat['time_sum'] += response_time
-            url_stat['time_max'] = max(url_stat['time_max'], response_time)
+            url_stats = analyzed_data.setdefault(request_url, initial_dict.copy())
+            url_stats['count'] += 1
+            url_stats['values'].append(response_time)
+            url_stats['time_max'] = max(url_stats['time_max'], response_time)
+            url_stats['time_sum'] = round(url_stats['time_sum'] + response_time, 3)
+            url_stats['time_avg'] = round(
+                ((url_stats['count'] - 1) * url_stats['time_avg'] + response_time) / url_stats['count'], 3
+            )
 
-    return analyzed_data
+    return analyzed_data, item_counter, time_counter
 
-def main():
-    config = get_config()
+
+def get_clear_analyzed_data(raw_analyzed_data: dict, item_counter: int, time_counter: float):
+    for url, stats in sorted(raw_analyzed_data.items(), key=lambda kv: kv[1]['time_sum'], reverse=True):
+        yield {
+            'url': url,
+            'count': stats['count'],
+            'time_max': stats['time_max'],
+            'time_sum': stats['time_sum'],
+            'time_avg': stats['time_avg'],
+            'time_med': median(stats['values']),
+            'time_perc': round(stats['time_sum'] / time_counter, 3),
+            'count_perc': round(stats['count'] / item_counter, 3),
+        }
+
+
+def create_report(analyzed_data: list[dict], report_filepath: str) -> None:
+    with open('report.html', 'r') as template_file, open(report_filepath, 'a') as report_file:
+        template = template_file.read()
+        rendered_template = template.replace('$table_json', str(analyzed_data))
+        report_file.write(rendered_template)
+
+
+def main(config: dict) -> None:
+    config = get_config(config)
 
     log_dir = config.get('LOG_DIR')
+    report_dir = config.get('REPORT_DIR')
+    report_size = config.get('REPORT_SIZE')
     log_filename = config.get('LOG_FILENAME')
 
     set_logging_config(log_filename)
-    log_file = get_last_log_file(log_dir)
-    analyzed_data = analyse_log(log_dir, log_file)
+
+    if log_file := get_last_log_file(log_dir):
+        report_filepath = os.path.join(report_dir, f'report-{log_file.date}.html')
+        if not os.path.exists(report_filepath):
+            raw_analyzed_data, item_counter, time_counter = get_raw_log_analysis(log_dir, log_file)
+            data_generator = get_clear_analyzed_data(raw_analyzed_data, item_counter, time_counter)
+            analyzed_data = (
+                list(data_generator)
+                if report_size >= item_counter
+                else [next(data_generator) for _ in range(report_size)]
+            )
+            create_report(analyzed_data, report_filepath)
+        else:
+            logger.info('Last log file has already been analyzed')
+    else:
+        logger.info('There is no required file for log analysis')
 
 
 if __name__ == "__main__":
-    main()
+    main(config=config)
