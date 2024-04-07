a7c926a572dd098056062f6b6b7cd146a5539506
Egor Kuritsin <ekuritsin@ptsecurity.com>
2024-04-07 22:34:19 +0300

    path fix

--- log_analyzer/tests/test_log_analyzer.py
+++ log_analyzer/tests/test_log_analyzer.py
@@ -8,18 +8,18 @@ from log_analyzer import (
 
 class LogAnalyzerTest(unittest.TestCase):
     def test_empty_log_dir(self):
-        self.assertIsNone(get_analyzed_log('./log_dirs/empty_log_dir'))
+        self.assertIsNone(get_analyzed_log('./tests/log_dirs/empty_log_dir'))
 
     def test_wrong_log_files(self):
-        self.assertIsNone(get_analyzed_log('./log_dirs/wrong_log_files'))
+        self.assertIsNone(get_analyzed_log('./tests/log_dirs/wrong_log_files'))
 
     def test_analyzed_log_exist(self):
-        actual = get_analyzed_log('./log_dirs/analyzed_log_exist')
+        actual = get_analyzed_log('./tests/log_dirs/analyzed_log_exist')
         expected = LogFile('log-20220630.gz', '20220630', 'gz')
         self.assertEqual(actual, expected)
 
     def test_several_log_files(self):
-        actual = get_analyzed_log('./log_dirs/several_log_files')
+        actual = get_analyzed_log('./tests/log_dirs/several_log_files')
         expected = LogFile('log-20220630.gz', '20220630', 'gz')
         self.assertEqual(actual, expected)
 
