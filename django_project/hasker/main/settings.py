bcd4ca64f0e8ad19d0a09a6d8dd1c365b9e0e06b
Egor Kuritsin <ekuritsin@ptsecurity.com>
2024-05-31 09:36:01 +0300

    action fix

--- django_project/hasker/main/settings.py
+++ django_project/hasker/main/settings.py
@@ -43,6 +43,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "main.urls"
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
