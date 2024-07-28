979e7456e34c55c579ca175dc41af4b4e8eab10c
Egor Kuritsin <ekuritsin@ptsecurity.com>
2024-07-28 22:58:06 +0300

    fix lint

--- concurrency/app.py
+++ concurrency/app.py
@@ -4,8 +4,8 @@ from threading import (
     Thread,
 )
 
-
 lock = Lock()
+
 
 def print_nums_1() -> None:
     """Вывод чисел от 1 до 10"""
@@ -13,29 +13,35 @@ def print_nums_1() -> None:
         time.sleep(0.1)
         print(i)
 
+
 def print_nums_2() -> None:
     """Вывод чисел от 1 до 10 с блокировкой"""
     with lock:
         print_nums_1()
 
+
 def build_threads_1() -> list[Thread]:
     """Возвращает список потоков"""
     return [Thread(target=print_nums_1) for _ in range(5)]
 
+
 def build_threads_2() -> list[Thread]:
     """Возвращает список потоков с блокировкой"""
     return [Thread(target=print_nums_2) for _ in range(5)]
+
 
 def run_threads(threads: list[Thread]) -> None:
     """Запуск потоков"""
     for thread in threads:
         thread.start()
 
+
 def wait_threads(threads: list[Thread]) -> None:
     """Ожидание завершения выполнения потоков"""
     for thread in threads:
         thread.join()
     print('Все потоки успешно выполнились!')
+
 
 # Запуск потоков, работающих одновременно
 threads = build_threads_1()
